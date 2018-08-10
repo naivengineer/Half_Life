b439a1963da2b79b8dfd0d1b61117f2b476b3e06
naivengineer <1530183575@qq.com>
2018-08-10 17:21:19 +0800

    更新 Model

--- moment/models.py
+++ moment/models.py
@@ -5,6 +5,26 @@ class Users(models.Model):
     user_id = models.IntegerField(default=0)
     user_name = models.CharField(max_length=200)
     pass_word = models.CharField(max_length=200)
-    
+    e_mail = models.CharField(max_length=200)
     def __str__(self):
         return self.user_name
+          
+class Moment(models.Model):
+    moment_id = models.IntegerField(default=0)
+    moment_date = models.DateTimeField('date published')
+    moment_text = models.CharField(max_length=2000)
+    
+class Message(model.Model):
+    message_id = models.IntegerField(default=0)
+    message_from = models.IntegerField(default=0)
+    message_to = models.IntegerField(default=0)
+    message_text = models.CharField(max_length=2000)
+    
+class user_info(models.Model):
+    user_info_id = models.IntegerField(default=0)
+    sex = models.CharField(max_length=2)
+    birthday = models.DateTimeField('date published')
+    hometown = models.CharField(max_length=200)
+    
+    
+    
